fb1b086ee03f11cbe272f14e61f9d45b3fae3af4
bahri hrf <bahrihrf34@gmail.com>
2024-10-23 23:10:46 +0300

    edit: request.options | create: api.config.ts

---
+++ src/config/api.config.ts
@@ -0,0 +1,6 @@
+const config = {
+    BASE_URL: "http://crmsntrl.netgsm.com.tr:9111",
+  };
+  
+export default config;
+  
---
+++ src/options/request.option.ts
@@ -0,0 +1,5 @@
+export interface RequestOptions {
+  username: string;
+  password: string;
+  baseUrl?: string;
+}
